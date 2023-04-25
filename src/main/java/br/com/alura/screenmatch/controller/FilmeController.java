8e4d6a23ccadb6d1e5f472837aaccdaa4970c679
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-04-25 16:38:41 -0300

    Cadastrando filmes

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,14 +1,20 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
+
+    private List<Filme> filmes = new ArrayList<>();
 
     @GetMapping
     public String carregaPaginaFormulario() {
@@ -17,7 +23,11 @@ public class FilmeController {
 
     @PostMapping
     public String cadastraFilme(DadosCadastroFilme dados) {
-        System.out.println(dados);
+        var filme = new Filme(dados);
+        filmes.add(filme);
+
+        System.out.println(filmes);
+
         return "filmes/formulario";
     }
 
