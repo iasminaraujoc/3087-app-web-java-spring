9eb6ee2fb48206f4c3a2d658875553c1cb6fdb16
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-04-27 16:15:37 -0300

    Botão para excluir filme

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.domain.filme.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,13 @@ public class FilmeController {
         var filme = new Filme(dados);
 
         repository.save(filme);
+
+        return "redirect:/filmes";
+    }
+
+    @DeleteMapping
+    public String removeFilme() {
+        System.out.println("FILME EXCLUIDO!");
 
         return "redirect:/filmes";
     }
