3e073ae0d593bca7914a8a3ba6ab093a2c9c2cd3
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-04-27 16:58:38 -0300

    Alterando dados

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,15 +1,14 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.domain.filme.DadosAlteracaoFilme;
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screenmatch.domain.filme.Filme;
 import br.com.alura.screenmatch.domain.filme.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +36,7 @@ public class FilmeController {
     }
 
     @PostMapping
+    @Transactional
     public String cadastraFilme(DadosCadastroFilme dados) {
         var filme = new Filme(dados);
 
@@ -45,7 +45,17 @@ public class FilmeController {
         return "redirect:/filmes";
     }
 
+    @PutMapping
+    @Transactional
+    public String alteraFilme(DadosAlteracaoFilme dados) {
+        var filme = repository.getReferenceById(dados.id());
+        filme.atualizaDados(dados);
+
+        return "redirect:/filmes";
+    }
+
     @DeleteMapping
+    @Transactional
     public String removeFilme(Long id) {
         repository.deleteById(id);
 
