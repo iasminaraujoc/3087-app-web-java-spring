521c91f48f5fba8f23eb987a904bcabb0828ce61
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2023-05-22 16:28:33 -0300

    aula 2

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -40,6 +40,26 @@ public class FilmeController {
         return "filmes/listagem";
     }
 
+    @GetMapping("/filtro")
+    public String carregaPaginaFiltro(Model model, Long idGenero, String tipoOrdenacao) {
+        model.addAttribute("generos", generoRepository.findAll());
+
+        if(idGenero != null){
+            var genero = generoRepository.getReferenceById(idGenero);
+            List<Filme> listaFiltrada;
+
+            if(tipoOrdenacao.equals("ano")){
+                listaFiltrada = repository.findByGeneroOrderByAnoLancamento(genero);
+            } else{
+                listaFiltrada = repository.findByGeneroOrderByNome(genero);
+            }
+
+            model.addAttribute("listaFiltrada", listaFiltrada);
+
+        }
+        return "filmes/filtro";
+    }
+
     @PostMapping
     @Transactional
     public String cadastraFilme(DadosCadastroFilme dados) {
--- src/main/java/br/com/alura/screenmatch/domain/filme/FilmeRepository.java
+++ src/main/java/br/com/alura/screenmatch/domain/filme/FilmeRepository.java
@@ -1,6 +1,13 @@
 package br.com.alura.screenmatch.domain.filme;
 
+import br.com.alura.screenmatch.domain.genero.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+    List<Filme> findByGeneroOrderByAnoLancamento(Genero genero);
+
+    List<Filme> findByGeneroOrderByNome(Genero genero);
 }
